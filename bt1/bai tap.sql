69c7b903cb4d7f3d03958b011a97a40da3616c57
pthinh123ts-dotcom <pthinh123.ts@gmail.com>
2025-11-20 18:23:59 +0700

    20112025_1

--- bt1/bai tap.sql
+++ bt1/bai tap.sql
@@ -2,44 +2,18 @@ DROP DATABASE IF EXISTS TestingSystem;
 CREATE DATABASE TestingSystem;
 USE TestingSystem;
 
-
-
-
-
 -- Tạo table 1: Department
-
 DROP TABLE IF EXISTS Department;
 CREATE TABLE Department(
 	DepartmentID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    DepartmentName 			VARCHAR(30) NOT NULL
+    DepartmentName 			NVARCHAR(30) NOT NULL
 );
 
--- Tạo table 2: Posittion
-
-DROP TABLE IF EXISTS Position;
-CREATE TABLE `Position` (
-	PositionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    PositionName			ENUM('Dev','Test','Scrum Master','PM') NOT NULL
-);
-
--- Tạo table 3: Account
 
 
-
-DROP TABLE IF EXISTS `Account`;
-CREATE TABLE `Account`(
-	AccountID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Email					VARCHAR(50) NOT NULL UNIQUE KEY,
-    Username				VARCHAR(50) NOT NULL UNIQUE KEY,
-    FullName				VARCHAR(50) NOT NULL,
-    DepartmentID 			TINYINT UNSIGNED NOT NULL,
-    PositionID				TINYINT UNSIGNED NOT NULL,
-    CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID) ,
-    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID) 
-);
 INSERT INTO Department(DepartmentName) 
-VALUES                  (N'Marketing'	),
+VALUES
+						(N'Marketing'	),
 						(N'Sale'		),
 						(N'Bảo vệ'		),
 						(N'Nhân sự'		),
@@ -49,7 +23,50 @@ VALUES                  (N'Marketing'	),
 						(N'Giám đốc'	),
 						(N'Thư kí'		),
 						(N'Bán hàng'	);
-    
+
+-- Tạo table 2: Posittion
+DROP TABLE IF EXISTS Position;
+CREATE TABLE `Position` (
+	PositionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    PositionName			ENUM('Dev','Test','Scrum Master','PM') NOT NULL
+);
+
+
+INSERT INTO Position	(PositionName	) 
+VALUES 					('Dev'			),
+						('Test'			),
+						('Scrum Master'	),
+						('PM'			); 
+
+
+-- Tạo table 3: Account
+DROP TABLE IF EXISTS `Account`;
+CREATE TABLE `Account`(
+	AccountID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Email					VARCHAR(50) NOT NULL UNIQUE KEY,
+    Username				VARCHAR(50) NOT NULL UNIQUE KEY,
+    FullName				NVARCHAR(50) NOT NULL,
+    DepartmentID 			TINYINT UNSIGNED NOT NULL,
+    PositionID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID) ON DELETE CASCADE,
+    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID) ON DELETE CASCADE
+);
+
+
+
+INSERT INTO `Account`(Email								, Username			, FullName				, DepartmentID	, PositionID, CreateDate)
+VALUES 				('Email1@gmail.com'				, 'Username1'		,'Fullname1'				,   '5'			,   '1'		,'20-11-2025'),
+					('Email2@gmail.com'				, 'Username2'		,'Fullname2'				,   '1'			,   '2'		,'20-11-2025'),
+                    ('Email3@gmail.com'				, 'Username3'		,'Fullname3'				,   '2'			,   '2'		,'20-11-2025'),
+                    ('Email4@gmail.com'				, 'Username4'		,'Fullname4'				,   '3'			,   '4'		,'20-11-2025'),
+                    ('Email5@gmail.com'				, 'Username5'		,'Fullname5'				,   '4'			,   '4'		,'20-11-2025'),
+                    ('Email6@gmail.com'				, 'Username6'		,'Fullname6'				,   '6'			,   '3'		,'20-11-2025'),
+                    ('Email7@gmail.com'				, 'Username7'		,'Fullname7'				,   '2'			,   '2'		, '20-11-2025'	),
+                    ('Email8@gmail.com'				, 'Username8'		,'Fullname8'				,   '8'			,   '1'		,'20-11-2025'),
+                    ('Email9@gmail.com'				, 'Username9'		,'Fullname9'				,   '2'			,   '2'		,'20-11-2025'),
+                    ('Email10@gmail.com'			, 'Username10'		,'Fullname10'				,   '10'		,   '1'		,'20-11-2025');
+
 
 
 -- Tạo table 4: Group
@@ -59,8 +76,23 @@ CREATE TABLE `Group`(
     GroupName				NVARCHAR(50) NOT NULL UNIQUE KEY,
     CreatorID				TINYINT UNSIGNED,
     CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId) 
+    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId) ON DELETE CASCADE
 );
+
+
+
+
+INSERT INTO `Group`	(  GroupName			, CreatorID		, CreateDate)
+VALUES 				(N'Testing System'		,   5			,'20-11-2025'),
+					(N'Development'			,   1			,'20-11-2025'),
+                    (N'VTI Sale 01'			,   2			,'20-11-2025'),
+                    (N'VTI Sale 02'			,   3			,'20-11-2025'),
+                    (N'VTI Sale 03'			,   4			,'20-11-2025'),
+                    (N'VTI Creator'			,   6			,'20-11-2025'),
+                    (N'VTI Marketing 01'	,   7			,'20-11-2025'),
+                    (N'Management'			,   8			,'20-11-2025'),
+                    (N'Chat with love'		,   9			,'20-11-2025'),
+                    (N'Ai'			        ,   10			,'20-11-2025');
 
 -- Tạo table 5: GroupAccount
 DROP TABLE IF EXISTS GroupAccount;
@@ -68,13 +100,93 @@ CREATE TABLE GroupAccount(
 	GroupID					TINYINT UNSIGNED NOT NULL,
     AccountID				TINYINT UNSIGNED NOT NULL,
     JoinDate				DATETIME DEFAULT NOW(),
-
+    PRIMARY KEY (GroupID,AccountID),
     FOREIGN KEY(GroupID) 		REFERENCES `Group`(GroupID) ON DELETE CASCADE
 );
+
+
+INSERT INTO `GroupAccount`	(  GroupID	, AccountID	, JoinDate	 )
+VALUES 						(	1		,    1		,'2019-03-05'),
+							(	1		,    2		,'2020-03-07'),
+							(	3		,    3		,'2020-03-09'),
+							(	3		,    4		,'2020-03-10'),
+							(	5		,    5		,'2020-03-28'),
+							(	1		,    3		,'2020-04-06'),
+							(	1		,    7		,'2020-04-07'),
+							(	8		,    3		,'2020-04-08'),
+							(	1		,    9		,'2020-04-09'),
+							(	10		,    10		,'2020-04-10');
+
 
 -- Tạo table 6: TypeQuestion
 DROP TABLE IF EXISTS TypeQuestion;
 CREATE TABLE TypeQuestion (
     TypeID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    TypeName 		ENUM UNIQUE KEY NOT NULL
+    TypeName 		ENUM('Essay','Multiple-Choice') NOT NULL UNIQUE KEY
 );
+
+-- Tạo table 7: CategoryQuestion
+DROP TABLE IF EXISTS CategoryQuestion;
+CREATE TABLE CategoryQuestion(
+    CategoryID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CategoryName			NVARCHAR(50) NOT NULL UNIQUE KEY
+);
+
+-- Tạo table 8: Question
+DROP TABLE IF EXISTS Question;
+CREATE TABLE Question(
+    QuestionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content					NVARCHAR(100) NOT NULL,
+    CategoryID				TINYINT UNSIGNED NOT NULL,
+    TypeID					TINYINT UNSIGNED NOT NULL,
+    CreatorID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CategoryID) 	REFERENCES CategoryQuestion(CategoryID) ON DELETE CASCADE,
+    FOREIGN KEY(TypeID) 		REFERENCES TypeQuestion(TypeID) ON DELETE CASCADE,
+    FOREIGN KEY(CreatorID) 		REFERENCES `Account`(AccountId) ON DELETE CASCADE 
+);
+
+-- Tạo table 9: Answer
+DROP TABLE IF EXISTS Answer;
+CREATE TABLE Answer(
+    AnswerID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content					NVARCHAR(100) NOT NULL,
+    QuestionID				TINYINT UNSIGNED NOT NULL,
+    isCorrect				BIT DEFAULT 1,
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID) ON DELETE CASCADE
+);
+
+
+-- Tạo table 10: Exam
+DROP TABLE IF EXISTS Exam;
+CREATE TABLE Exam(
+    ExamID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `Code`					CHAR(10) NOT NULL,
+    Title					NVARCHAR(50) NOT NULL,
+    CategoryID				TINYINT UNSIGNED NOT NULL,
+    Duration				TINYINT UNSIGNED NOT NULL,
+    CreatorID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CategoryID) REFERENCES CategoryQuestion(CategoryID) ON DELETE CASCADE,
+    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId) ON DELETE CASCADE
+);
+
+-- Tạo table 11: ExamQuestion
+DROP TABLE IF EXISTS ExamQuestion;
+CREATE TABLE ExamQuestion(
+    ExamID				TINYINT UNSIGNED NOT NULL,
+	QuestionID			TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID) ON DELETE CASCADE,
+    FOREIGN KEY(ExamID) REFERENCES Exam(ExamID) ON DELETE CASCADE,
+    PRIMARY KEY (ExamID,QuestionID)
+);
+ 
+
+
+
+
+
+
+
+
+
